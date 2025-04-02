b7d40f50c8b7e0bd1aceb337769710fef99e5347
CleuJunior <cleu.junior@gmail.com>
2025-04-02 00:38:12 -0300

    git initial project

---
+++ src/main/java/br/com/uber/FoodTrucksApplication.java
@@ -0,0 +1,13 @@
+package br.com.uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTrucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTrucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uber/FoodTrucksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTrucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
